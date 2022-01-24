944347835fe4b9ca1c26c97d0687670f9b378ba7
Bart Huiskamp <bartolameus.h@gmail.com>
2022-01-24 12:09:26 +0100

    platform gravity

--- assets/mechanics.js
+++ assets/mechanics.js
@@ -5,39 +5,53 @@ const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
 
 let gravity = 0; //here i can toy around with the gravity
-let floorObstacleArr = []
+
+
 
 const ball = {
-  x: 100,// placement on the canvas
-  y: 30,
-  vx: 0, // this is speed in the x axis of the pull (maby future game mechanic)
-  vy: 2,
-  radius: 10,
-  color: "#2e7d32",
-  userPull: 0,
-  draw: function () {
-    ctx.beginPath();
-    ctx.arc(this.x, this.y, this.radius, 0,
-         Math.PI * 2, true);
-    ctx.closePath();
-    ctx.fillStyle = this.color;
-    ctx.fill();
-  },
-};
+    x: 100,// placement on the canvas
+    y: 30,
+    vx: 0, // this is speed in the x axis of the pull (maby future game mechanic)
+    vy: 0.7,
+    width: 10,
+    height: 10,
+    color: "#2e7d32",
+    userPull: 0,
+    
+    draw: function () {
+        ctx.beginPath();
+        ctx.rect(this.x, this.y, this.width, this.height);
+            ctx.closePath();
+            ctx.fillStyle = this.color;
+            ctx.fill();
+        },
+    };
+    
+    console.log(ball.y)
+    
+    
+    let floorObstacleArr = []
+    // createFloor = new obstacle(floorObstacle.floorX, floorObstacle.floorY, floorObstacle.floorcolor, floorObstacle.floorWidth, floorObstacle.floorHeight);
 
 const floorObstacle = {
-    floorWidth: 100,
-    floorHeight: 20,
+    floorX: 50,
+    floorY: 100,
+    floorWidth: 150,      // turn into canvas width for total length
+    floorHeight: 5,
     floorPadding: 10,
+    floorcolor: "#0095DD",
     
     drawFloor: function () {
         ctx.beginPath();
-        ctx.rect(0, 0, canvas.width, this.floorHeight );
-        ctx.fillStyle = "#0095DD";
+        ctx.rect(this.floorX, this.floorY, this.floorWidth, this.floorHeight);
+        ctx.fillStyle = this.floorcolor
         ctx.fill();
         ctx.closePath();
     },
 }
+
+
+
 
 
 ball.draw();
@@ -46,14 +60,14 @@ function update() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   ball.draw();
   ball.vy += gravity - ball.userPull;
-  ball.x += ball.vx;
+  ball.x += ball.vx; // the width and height
   ball.y += ball.vy;
   floorObstacle.drawFloor();
   
     if(rightPressed) {
         ball.x += 7;
-        if (ball.x + ball.radius > canvas.width){
-            ball.x = canvas.width - ball.radius;
+        if (ball.x + ball.width > canvas.width){
+            ball.x = canvas.width - ball.width;
         }
     }
     else if(leftPressed) {
@@ -62,18 +76,18 @@ function update() {
             ball.x = 0;
         }
     }
-
+   
+    hitBottom();  // conditions wen ball hit bottem of canvis
+    //wil be need for every slider.
     
+    floorcollision(); // these condition make sure thea are repeated in this function
 
-  hitBottom();  // conditions wen ball hit bottem of canvis
-  //wil be need for every slider.
-
-
+    // hitFloor();
 }
 
 
 
-// trying to get controles right.
+
 let rightPressed = false;
 let leftPressed = false;
 
@@ -97,9 +111,43 @@ function keyUpHandler(e) {
         leftPressed = false;
     }
 }
+function floorcollision () {
+   let x = floorObstacle.floorX;
+   let y = floorObstacle.floorY;
+   let w =floorObstacle.floorWidth;
+   
+
+    if (!(ball.x > x + w ||
+        ball.x + ball.width < x ||
+        ball.y > y + floorObstacle.floorHeight ||
+        ball.y + ball.height < floorObstacle.floorHeight))
+        {
+        ball.color = "#ff0000";
+        console.log("collison")
+        let endfloor = floorObstacle.floorY - ball.height;
+    if (ball.y > endfloor) {
+        ball.y = endfloor;
+    } 
+    } else {
+        console.log(" no collison")
+       ball.color = "#2e7d32";
+    }
+       
+    
+}
+
+function hitFloor() {
+    
+    let endfloor = floorObstacle.floorY - ball.height;
+    if (ball.y > endfloor) {
+        ball.y = endfloor;
+    } 
+
+    
+}
 
 function hitBottom() {
-    let rockbottom = canvas.height - ball.radius;
+    let rockbottom = canvas.height - ball.height;
     if (ball.y > rockbottom) {
         ball.y = rockbottom;
         clearInterval();
